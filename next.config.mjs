c9eed309a90c893756e83ffc8f6484f0c22e22bd
vrajdarji21 <vraj@gohilinfotech.com>
2026-04-23 12:44:44 +0530

    minor chnage done

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5050/api';
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5050/api/:path*',
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
